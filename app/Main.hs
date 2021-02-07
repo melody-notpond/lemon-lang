16461ff19c128d807f5e256567abf27f2ab1d3be
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-02-07 12:53:52 -0500

    Implemented parsing numbers and characters

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lemon.Data
 
 main :: IO ()
-main = print $ SExpr [Atom "bindr", Quote (Atom "list"), SExpr [Atom "func", Quote (Atom "a"), SExpr [Atom "func", Quote (Atom "b"), SExpr [Atom "func", Quote (Atom "f"), SExpr [Atom "f", Atom "a", Atom "b"]]]]]
+main = putStrLn "hewwo wowwd"
